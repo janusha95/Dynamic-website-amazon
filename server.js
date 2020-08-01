51a63e69d3251dbd508b3e232ff4c2b05f3f90b9
janusha95 <60120739+janusha95@users.noreply.github.com>
2020-08-01 00:44:46 -0400

    connected to mongo db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 require('dotenv').config({path:"./config/keys.env"});
 
@@ -22,6 +23,13 @@ const productController = require("./controllers/product");
 
 app.use("/", generalController);
 app.use("/products", productController);
+
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log(`Connected to Mongodb database`);
+})
+.catch(err=>console.log(`Error when connecting to database ${err}`));
 
 app.listen(PORT, ()=>{
     console.log("Web server is running");
